7e008910907f97907aa7fec8322a84987d94759a
marco-V-99 <64664178+marco-V-99@users.noreply.github.com>
2025-09-19 20:17:44 -0300

    Practica_3.py

--- Practica_3.py
+++ Practica_3.py
@@ -8,7 +8,7 @@ def mayor():
     else:
         mayor = print(f"a: ({a}) es mayor a b: ({b})" if a > b else f"b:({b}) es mayor que a:({a})") 
 
-#mayor()
+mayor()
 
 #2 Buscar una palabra en una lista ingresada por teclado usando args y un operador ternario
 
@@ -17,17 +17,18 @@ def find_word(f_arg,*argv):
     for arg in argv:
         print (f"Se encontro la palabra: {entrada}" if entrada == arg else "no se encontro la palabra")
 
-#find_word("python", "foo", "bar", "hola", "perro", "sandia", "fighters")
+find_word("python", "foo", "bar", "hola", "perro", "sandia", "fighters")
 
 #3 Determinar si un número es par o impar
 def num_par():
     a = int(input("Introduzca un numero: "))
     print(f"El numero es par" if a % 2 == 0 else "El numero es inpar")
 
-#num_par()
+num_par()
 
 #4 Calcular el promedio de una lista de números usando args y un operador ternario
 
     
 #5 Imprimir un mensaje de error si no se pasan suficientes argumentos
+
 
